73b094b308266fc7743b999d0f00e1219471d8c3
BenbounBigHydra <benoit.jaques@sunrise.ch>
2026-06-05 11:32:38 +0200

    putain de majuscule

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Company;
 use App\Models\Collection;
-use App\services\CompanyStatsService;
-use App\services\AdminService;
+use App\Services\CompanyStatsService;
+use App\Services\AdminService;
 use Illuminate\Support\Carbon;
 
 class AdminController extends Controller
