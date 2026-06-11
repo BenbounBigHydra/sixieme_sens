9a02b64288a6a4a1f6c2a2315df593a3b2784f4a
BenbounBigHydra <benoit.jaques@sunrise.ch>
2026-06-11 16:44:44 +0200

    menu fixed

--- resources/js/components/HeaderCobrand.vue
+++ resources/js/components/HeaderCobrand.vue
@@ -28,7 +28,7 @@
 
       <!-- Mobile menu button -->
       <div v-if="!isClosedPage" class="md:hidden flex items-center">
-        <button @click="isMenuOpen = !isMenuOpen" class="text-[#0073E6] focus:outline-none">
+        <button @click="isMenuOpen = !isMenuOpen" class="text-[#0073E6] focus:outline-none transition-transform duration-300" :class="{ 'rotate-90': isMenuOpen }">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
